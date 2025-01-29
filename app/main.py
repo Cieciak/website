0b185f8ad1ac0dc9cc29b7487275d6a69df42621
Cieciak <nikodemrzasa@wp.pl>
2025-01-29 01:10:14 +0100

    Add base homepage

--- app/main.py
+++ app/main.py
@@ -6,3 +6,7 @@ main = Blueprint('main', __name__)
 @main.route('/')
 def index():
     return render_template('index.html')
+
+@main.route('/home')
+def homepage():
+    return render_template('homepage.html')
