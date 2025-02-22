3e4174d0bc47093c751482acf12b98589fc73bc7
Cieciak <nikodemrzasa@wp.pl>
2025-02-22 13:57:37 +0100

    Second work day

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 from flask import Blueprint
 from flask import render_template
 
+import os
+
 main = Blueprint('main', __name__)
 
 @main.route('/')
@@ -9,4 +11,18 @@ def index():
 
 @main.route('/home')
 def homepage():
-    return render_template('homepage.html')
+
+    # Get all projects
+    PROJ_ROOT = 'app/templates/projects/'
+    projects = []
+    for proj_path in os.listdir(PROJ_ROOT):
+        if 'desc.txt' in os.listdir(os.path.join(PROJ_ROOT, proj_path)):
+
+            with open(os.path.join(PROJ_ROOT, proj_path, 'desc.txt')) as file:
+                title, description = file.readlines()
+            
+            projects.append(
+                (title, description)
+            )
+
+    return render_template('homepage.html', projects=projects)
