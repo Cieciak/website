0aa5b1740248298091b2a550010784700196c50b
Cieciak <nikodemrzasa@wp.pl>
2025-05-21 00:05:26 +0200

    News template

--- app/static/js/news.js
+++ app/static/js/news.js
@@ -52,13 +52,62 @@ function deleteSelf(element){
     element.parentElement.remove();
 }
 
-function allowDrop(ev){
-    ev.preventDefault();
+function eventDragover(event){
+    event.stopPropagation();
+    event.preventDefault();
+
+    event.dataTransfer.dropEffect = 'copy';
 }
 
-function dropHandle(ev){
-    ev.preventDefault();
-    ev.stopPropagation();
+async function appendGallery(event){
+    event.stopPropagation();
+    event.preventDefault();
 
-    console.log('hello');
+    let gallery = document.getElementById("img-list");
+
+    for (const file of event.dataTransfer.files){
+        let element = document.createElement("div");
+
+        let name    = document.createElement("p");
+        name.innerText = file.name;
+        element.appendChild(name);
+
+        let image   = document.createElement("img");
+        image.src = "data:image/png;base64," + await getBase64(file);
+        element.appendChild(image);
+
+        gallery.appendChild(element);
+    }
+}
+
+async function eventDrop(event){
+    event.stopPropagation();
+    event.preventDefault();
+
+    let files = event.dataTransfer.files;
+    console.log(files);
+    let file  = files[0];
+
+    let data = await getBase64(file);
+
+    let image = document.getElementById("img-title");
+    image.src = 'data:image/png;base64,' + data;
+
+    var image_title = document.getElementById("p-img-title");
+    image_title.innerText = file.name;
+}
+
+async function getBase64(file){
+    return new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = () => {
+            let encoded = reader.result.toString().replace(/^data:(.*,)?/, '');
+            if ((encoded.length % 4) > 0) {
+                encoded += '='.repeat(4 - (encoded.length % 4));
+            }
+            resolve(encoded);
+        };
+        reader.onerror = error => reject(error);
+    });
 }
